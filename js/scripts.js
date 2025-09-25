e16cd53c9ab5409749eb30e1abe76242de348787
Francisco Garroni <chicogarroni@gmail.com>
2025-09-25 09:01:27 -0300

    refactor: remove console.log do javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
 	const mobileMenu = document.querySelector(".mobile-menu");
 	const navlinks = document.querySelectorAll(".mobile-menu .nav-link");
 
-	console.log("Botão do menu:", menuButton);
-	console.log("Menu Mobile:", mobileMenu);
-	console.log("Links do menu:", navlinks);
-	console.log("Quantidade de links:", navlinks.length);
-
 	menuButton.addEventListener("click", () => {
-		console.log("Botão foi clicado!");
 		mobileMenu.classList.toggle("active");
-		console.log("Classe active adicionada/removida");
 	});
 
 	navlinks.forEach((link) => {
 		link.addEventListener("click", () => {
 			mobileMenu.classList.remove("active");
-			console.log("Menu fechado ao clicar no link:", link.textContent);
 		});
 	});
 
-	document.querySelectorAll("a[href^='#']").forEach(anchor => {
-		anchor.addEventListener("click", function(e) {
+	document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+		anchor.addEventListener("click", function (e) {
 			e.preventDefault();
 			const target = document.querySelector(this.getAttribute("href"));
 			if (target) {
